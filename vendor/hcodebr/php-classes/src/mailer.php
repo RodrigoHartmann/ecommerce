48cd86c4a7fb9a134bb4a3663d0d2945267ce7d4
Rodrigo Hartmann Machado <61086426+RodrigoHartmann@users.noreply.github.com>
2020-03-13 09:59:43 -0300

    Update mailer.php

--- vendor/hcodebr/php-classes/src/mailer.php
+++ vendor/hcodebr/php-classes/src/mailer.php
@@ -7,7 +7,7 @@ use Rain\Tpl;
 class Mailer {
 	
 	const USERNAME = "rodrigohartmann182@gmail.com";
-	const PASSWORD = "184865*rdg";
+	const PASSWORD = "*************";
 	const NAME_FROM = "Hcode Store";
 
 
